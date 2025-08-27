b8085d5890b12b1493c883927515770c0ba0db62
sidarth16 <ssidarth1999@gmail.com>
2025-08-27 12:17:07 +0530

    config upd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push(
+        "@next/swc-linux-x64-gnu",
+        "@next/swc-linux-x64-musl"
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
